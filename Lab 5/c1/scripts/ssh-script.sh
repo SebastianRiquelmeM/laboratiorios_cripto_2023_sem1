7c51dd274028028036a91abc87760d58334d0afd
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-06-10 02:12:11 -0400

    ssh funciona automatizado, pero no logro capturar tráfico

--- Lab 5/c1/scripts/ssh-script.sh
+++ Lab 5/c1/scripts/ssh-script.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 # ssh-script.sh
 
+# The first argument is the IP address of the s1 container
+s1_ip=$1
+
+echo "Starting tcpdump..."
+
 # Start tcpdump capturing packets and send it to the background
-tcpdump -i any -w /captures/capture.pcap 'port 22 and host s1' &
+tcpdump -i any -w /captures/capture.pcap &
+
+# Get the PID of tcpdump
+PID=$!
+
+echo "Tcpdump started with PID $PID."
 
 # Attempt SSH connection
-sshpass -p 'test' ssh -o StrictHostKeyChecking=no test@s1
+echo "Attempting SSH connection..."
+sshpass -p 'test' ssh -o StrictHostKeyChecking=no test@$s1_ip
+
+echo "SSH connection attempted."
+
+# Kill tcpdump process and wait for it to finish writing the pcap file
+echo "Killing tcpdump..."
+kill $PID
+wait $PID
+
+echo "Tcpdump killed. Exiting script."
---
+++ Lab 5/main.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -x
+
+# Stop all running containers
+docker stop $(docker ps -aq)
+
+# Remove all containers
+docker rm $(docker ps -aq)
+
+# Remove all images
+docker rmi $(docker images -q)
+
+# Remove all networks
+docker network rm $(docker network ls -q)
+
+# Navigate to the s1 directory and build the image
+cd s1
+docker build -t my_image_s1 .
+
+cd ..
+
+# Navigate to the c1 directory and build the image
+cd c1
+docker build -t my_custom_image_c1 .
+
+# Create a network
+docker network create mynetwork
+
+# Run the s1 container in the background
+docker run --network=mynetwork -d --name s1 my_image_s1
+
+# Wait for a while to allow the s1 container to fully start
+sleep 10
+
+# Get the IP address of the s1 container
+s1_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' s1)
+
+# Run the c1 container in the background with elevated privileges, pass s1_ip as an argument to ssh-script.sh, wait for 20 seconds, then stop it
+timeout 20 docker run --network=mynetwork --privileged -v "$(pwd)/captures:/captures" --name c1 my_custom_image_c1 /ssh-script.sh $s1_ip
+
+# The script ends here
