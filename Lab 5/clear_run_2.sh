589b3be23cf4d07db2d49baf233d7ccd096fcbb9
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-06-10 03:09:55 -0400

    Se logra automatizar todo por primera vez

--- Lab 5/clear_run_2.sh
+++ Lab 5/clear_run_2.sh
@@ -29,8 +29,26 @@ docker network create mynetwork
 # Run the s1 container in the background
 docker run --network=mynetwork -d --name s1 my_image_s1
 
-# Run the my_custom_ubuntu container with tcpdump and SSH
-docker run --network=mynetwork -d -it --name my_custom_ubuntu my_custom_ubuntu /bin/bash -c "tcpdump -U -i any -w ./capture.pcap >/dev/null 2>&1 & sshpass -p 'test' ssh test@192.168.64.2"
+# Obtain the IP address of s1 container
+s1_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' s1)
+
+# Run the my_custom_ubuntu container
+docker run --network=mynetwork -d -it --name my_custom_ubuntu my_custom_ubuntu
+
+# Wait for 10 seconds
+sleep 10
+
+# Start tcpdump in the my_custom_ubuntu container
+docker exec -d my_custom_ubuntu /bin/bash -c 'tcpdump -U -i any -w ./capture.pcap >/dev/null 2>&1 &'
+
+# Wait for 10 seconds
+sleep 10
+
+# SSH into the my_custom_ubuntu container
+docker exec -it my_custom_ubuntu /bin/bash -c "sshpass -p 'test' ssh test@$s1_ip"
+
+# Wait for a few seconds to ensure tcpdump finishes capturing
+sleep 10
 
 # Copy capture.pcap from my_custom_ubuntu container to the host
-docker cp my_custom_ubuntu:./capture.pcap ./captura.pcap
+timeout 10 docker cp my_custom_ubuntu:./capture.pcap ./captura.pcap
