1eb5001e1dda693f8f5c8f9d4b9203fce29ce36c
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-06-10 01:13:18 -0400

    ssh funciona automatizado, pero no logro capturar tráfico

--- Lab 5/c1/scripts/ssh-script.sh
+++ Lab 5/c1/scripts/ssh-script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # ssh-script.sh
+
+# Start tcpdump capturing packets and send it to the background
+tcpdump -i any -w /captures/capture.pcap 'port 22 and host s1' &
+
+# Attempt SSH connection
 sshpass -p 'test' ssh -o StrictHostKeyChecking=no test@s1
